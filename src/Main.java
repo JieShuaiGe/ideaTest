26dd3949862593b36ad3ba0ef8c03becb7ad9218
XiangWenJie <1304056844@qq.com>
2018-04-20 09:38:11 +0800

    12121221

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+        System.out.println("你好呀!!!");
+        System.out.println("ewgwuefwehjbfewjfewugfew");
+        int i=1;
+        while (i<10){
+            i++;
+            System.out.println("哈哈哈"+i);
+        }
+
+    }
+
+    public String bb(){
+        System.out.println("124q35");
+        return "24356";
+    }
+
+    public static void main(String[] args) {
+        System.out.println("214235");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("ghjjdf");
+    }
+    public static void main(String[] args){
+
+    }
+}
